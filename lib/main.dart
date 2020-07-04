93d1ac249cd553a1bf588bdbd1524cbfd0154401
Andrei Keida <dronwork@outlook.com>
2020-07-04 16:14:06 +0300

    Init project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import './pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Main());
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        accentColor: Colors.redAccent,
+      ),
+      title: 'Using Provider',
+      home: HomePage(),
+    );
+  }
+}
